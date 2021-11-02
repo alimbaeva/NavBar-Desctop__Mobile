c3d6e21c8feda7706849ba64a80bec9f9dfb4054
Asel Alimbaeva <asel_19.86@mail.ru>
2021-11-02 17:57:17 +0300

    add code

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function () {
+
+    const burgerNav = this.document.querySelector('.nav-burger');
+    const navMenu = this.document.querySelector('.nav-menu');
+    const navMenu__link = this.document.querySelectorAll('.nav-menu__link');
+    const minBlock = this.document.querySelector('.min-block');
+    const link_1 = this.document.querySelector('.link_1');
+
+
+    burgerNav.addEventListener('click', () => {
+        navMenu.classList.toggle('displayBlock');
+    })
+
+    navMenu__link.forEach((link) => {
+        link.addEventListener("mouseenter", () => {
+            link.classList.toggle('click_li');
+        })
+        link.addEventListener("mouseleave", () => {
+            link.classList.toggle('click_li');
+        })
+    })
+
+})
